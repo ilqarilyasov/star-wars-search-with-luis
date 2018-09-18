7fa80e5278c5d8745b908f33208b5a1d49c27ff3
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-18 11:27:22 -0500

    TableView and TableeViewCell added

---
+++ Star Wars with Luis/Model/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  Star Wars with Luis
+//
+//  Created by Ilgar Ilyasov on 9/18/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let gender: String
+    let age: Int
+}
---
+++ Star Wars with Luis/View Controller/PersonTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonTableViewCell.swift
+//  Star Wars with Luis
+//
+//  Created by Ilgar Ilyasov on 9/18/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+    
+}
