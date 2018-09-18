247a61ab8c76978f607b4ae5a6a4beae275b0d0b
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-18 12:07:53 -0500

    PersonController partly completed

--- Star Wars with Luis/Model Controller/PersonController.swift
+++ Star Wars with Luis/Model Controller/PersonController.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+private let baseURL = URL(string: "https://swapi.co/api/people/")!
+
+class PersonController {
+    
+    private enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
+    
+    func searchForPeople(with searchTerm: String, completion: @escaping ([Person]?, Error?) -> Void){
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)!
+        let searchQueryItem = URLQueryItem(name: "search", value: searchTerm)
+        
+        urlComponents.queryItems = [searchQueryItem]
+        
+        guard let requestURL = urlComponents.url else {
+            NSLog("Problem constructing search url for: \(searchTerm)")
+            completion (nil, NSError())
+            return
+        }
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        
+        
+    }
+}
